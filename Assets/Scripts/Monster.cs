db41c55e16c079fbf84f3884fadb24156d026ea2
kimtaeyun0419 <sdh230104@sdh.hs.kr>
2024-04-08 20:48:48 +0900

    MonsterMove

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -7,10 +7,16 @@ public class Monster : MonoBehaviour
 {
     [SerializeField] int current_HP; // 몬스터 현재 체력
     [SerializeField] int max_HP; // 몬스터 총 체력
+    [SerializeField] float speed; // 몬스터의 움직임 속도
 
     [SerializeField] int attackPower; // 공격력
 
     [SerializeField] TextMeshProUGUI moster_HP; // 화면상에 나오는 몬스터의 체력
+
+
+    [Header("레이케스트")]
+    [SerializeField] float layLineSize;
+    [SerializeField] LayerMask isLayer;
 
     private void Start()
     {
@@ -19,6 +25,14 @@ public class Monster : MonoBehaviour
     private void Update()
     {
         moster_HP.text = ("남은 체력" + current_HP);
+        if (isPlayer())
+        {
+            transform.Translate(transform.right * -speed * Time.deltaTime);
+        }
+    }
+    private bool isPlayer()
+    {
+        return Physics2D.Raycast(transform.position, transform.right * -1 * layLineSize, isLayer);
     }
 
     public void takeDMG(int damage)
